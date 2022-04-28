03962b761e88e2306c778ec217e8b38fbd7fabfb
traciingu <traciinguyen@hotmail.com>
2022-04-28 15:50:47 -0400

    Refactored out comments

--- test/google.test.js
+++ test/google.test.js
@@ -11,23 +11,10 @@ describe('Google', () => {
         expect(listTitles).toEqual([ 'Todo', 'Done', 'In progress' ]);
         
         // Inside 'Todo' it contains a card named 'Hello'
-        //// Find the list with the list called "ToDo"
-        const lists = await page.$$('.list');
-        // Selects parent node of list
         const [parentListElement] = await page.$x('//div[text()="Hello"]/ancestor::div[@class="list"]')
-        // TODO: get the h2 text from parentListElement and assert that it is "ToDo"
-        // const titleHTML = await parentListElement.getProperty('innerHTML');
-        // const innerHTMLTitle = await titleHTML.jsonValue();
-        // console.log(innerHTMLTitle)
-
         const listHeader = await parentListElement.$eval('h2', node => node.innerText);
-        // const listHeaderText = await listHeader.innerText();
-        console.log(listHeader);
 
         expect(listHeader).toEqual('Todo');
-
-        // const todoList = lists.find(list => list.$eval('h2', node => node.innerText) === 'Todo')
-        // console.log(todoList)
 
     });
 });
