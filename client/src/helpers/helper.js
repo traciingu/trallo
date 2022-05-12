92e81ac44ab5ddd93be1ab49cd5777ab2cb4ec6a
traciingu <traciinguyen@hotmail.com>
2022-05-12 14:39:55 -0400

    Fixed first test, second test fails

--- client/src/helpers/helper.js
+++ client/src/helpers/helper.js
@@ -11,9 +11,11 @@ export const reorderLists = (listOrdering, draggable) => {
     return orderingCpy;
 };
 
-const reorderElements = (sourceArr, destinationArr, draggable) => {
-    sourceArr.splice(draggable.sourceIndex, 1);
-    destinationArr.splice(draggable.destinationIndex, 0, draggable.id);
+export const reorderElements = (sourceArr, destinationArr, draggable) => {
+    console.log("Source Arr: ", sourceArr);
+    console.log("Draggable: ", draggable);
+    sourceArr.splice(draggable.source.index, 1);
+    destinationArr.splice(draggable.destination.index, 0, draggable.id);
 
     return { sourceArr, destinationArr };
 };
@@ -23,17 +25,22 @@ const copyCollection = (obj, key) => {
 }
 
 export const reorderCards = (cardOrdering, draggable) => {
-    const {source, destination, id } = draggable;
-    const listIsNotEmpty = cardOrdering[destination.droppableId] && cardOrdering[source.droppableId];
+    const { source, destination, id } = draggable;
+    const sourceListIsNotEmpty = cardOrdering[source.droppableId].length > 0;
+    const destListIsNotEmpty = cardOrdering[destination.droppableId].length > 0;
     const listIsNotSame = destination.droppableId !== source.droppableId;
 
     if (listIsNotSame) {
         let destCpy = [];
         let sourceCpy = [];
 
-        if (listIsNotEmpty) {
-            destCpy = copyCollection(cardOrdering, destination.droppableId);
+        if (sourceListIsNotEmpty) {
             sourceCpy = copyCollection(cardOrdering, source.droppableId);
+            console.log(sourceCpy)
+        }
+
+        if(destListIsNotEmpty){
+            destCpy = copyCollection(cardOrdering, destination.droppableId);
         }
 
         reorderElements(sourceCpy, destCpy, { source, destination, id });
@@ -50,5 +57,22 @@ export const reorderCards = (cardOrdering, draggable) => {
 
         return destCpy;
     }
+};
+
+// TODO get rid of boolean parameter
+export function reorderInSameList(listIsNotEmpty, cardOrdering, source, destination, id) {
+    if (!cardOrdering[destination.droppableId].includes(id)) {
+        throw 'Id could not be found in array of ids';
+    }
+
+    let destCpy = [];
+
+    if (listIsNotEmpty) {
+        destCpy = copyCollection(cardOrdering, destination.droppableId);
+    }
+
+    reorderElements(destCpy, destCpy, { source, destination, id });
+
+    return destCpy;
 };
 
---
+++ client/src/helpers/helpers.test.js
@@ -0,0 +1,49 @@
+const { reorderInSameList, reorderElements } = require("./helper")
+
+describe('reorderInSameList', () => {
+    it('throws error when id does not exist in array of ids', () => {
+        // Arrange
+        const arrOfIds = { 1: [], 2: [], 3: [] };
+        const source = {};
+        const destination = { droppableId: 1, index: 1 };
+        const id = 1;
+
+        // Act
+        const shouldThrowErr = (() => reorderInSameList(true, arrOfIds, source, destination, id));
+
+        // Assert
+        expect(shouldThrowErr).toThrow(new Error('Id could not be found in array of ids'));
+    })
+
+    it('reorders card in same list', () => {
+        // Arrange
+        const arrOfIds = { 1: [1, 2], 2: [3, 4], 3: [5] };
+        const source = {};
+        const destination = { droppableId: 1, index: 1 };
+        const id = 1;
+
+        // Act
+        const result = reorderInSameList(true, arrOfIds, source, destination, id);
+
+        // Assert
+        expect(result).toEqual([2, 1]);
+    })
+});
+
+describe('reorderElements', () => {
+    it('moves a card from one list to another', () => {
+        // Arrange
+        const sourceArr = [1, 2];
+
+        const source = {droppableId: 1, index: 0};
+        const destination = { droppableId: 1, index: 1 };
+        const id = 1;
+
+        // Act
+        const {sourceArrResult, destArrResult} = reorderElements(sourceArr, sourceArr, {source, destination, id});
+
+        // Assert
+        expect(sourceArrResult).toEqual([2]);
+        
+    })
+})
